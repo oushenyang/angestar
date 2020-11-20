c1b83be3a94e3f3f42ca4de055b85a0e0671586c
shenyang.ou <shenyang.ou@jing-tong.com>
2020-11-20 16:54:32 +0800

    [FeaTrue]新增注册码卡类

--- ange-vip-main/src/main/webapp/assets/modular/agent/agentApp_card.js
+++ ange-vip-main/src/main/webapp/assets/modular/agent/agentApp_card.js
@@ -40,7 +40,7 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
 
     //一些事件监听
     element.on('tab(cardTabBrief)', function (data) {
-        if (data.index==0){
+        if (data.index == 0) {
             // 渲染表格
             table.render({
                 elem: '#' + AgentApp.card,
@@ -50,29 +50,29 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
                 defaultToolbar: [],
                 height: "full-126",
                 cellMinWidth: 100,
-                where:{
-                    'cardType':0,
-                    'agentAppId':Feng.getUrlParam("agentAppId")
+                where: {
+                    'cardType': 0,
+                    'agentAppId': Feng.getUrlParam("agentAppId")
                 },
                 cols: AgentApp.initColumn()
             });
         }
-        // else if(data.index==1){
-        //     // 渲染表格
-        //      table.render({
-        //         elem: '#' + AgentApp.currentCard,
-        //         url: Feng.ctxPath + '/agentCard/list',
-        //         page: true,
-        //         toolbar: '#' + AgentApp.currentCard + '-toolbar',
-        //         defaultToolbar: [],
-        //         height: "full-115",
-        //         cellMinWidth: 100,
-        //          where:{
-        //              'cardType':1,
-        //              'agentAppId':Feng.getUrlParam("agentAppId")
-        //          },
-        //         cols: AgentApp.initColumn()
-        //     });
+            // else if(data.index==1){
+            //     // 渲染表格
+            //      table.render({
+            //         elem: '#' + AgentApp.currentCard,
+            //         url: Feng.ctxPath + '/agentCard/list',
+            //         page: true,
+            //         toolbar: '#' + AgentApp.currentCard + '-toolbar',
+            //         defaultToolbar: [],
+            //         height: "full-115",
+            //         cellMinWidth: 100,
+            //          where:{
+            //              'cardType':1,
+            //              'agentAppId':Feng.getUrlParam("agentAppId")
+            //          },
+            //         cols: AgentApp.initColumn()
+            //     });
         // }
         else {
             // 渲染表格
@@ -84,9 +84,9 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
                 defaultToolbar: [],
                 height: "full-126",
                 cellMinWidth: 100,
-                where:{
-                    'cardType':2,
-                    'agentAppId':Feng.getUrlParam("agentAppId")
+                where: {
+                    'cardType': 2,
+                    'agentAppId': Feng.getUrlParam("agentAppId")
                 },
                 cols: AgentApp.initColumn()
             });
@@ -108,20 +108,15 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
                 }
             },
             {field: 'cardTypeName', align: 'center', title: '卡类名称'},
-            {field: 'marketPrice', align: 'center', edit: 'number', title: '市场价'},
-            {field: 'agentPrice', align: 'center', edit: 'number', title: '代理价'},
-            {field: 'createTime', align: 'center', width: 160,  sort: true, title: '创建时间'},
+            {field: 'marketPrice', align: 'center', title: '市场价',templet: '#marketPriceEditTpl'},
+            {field: 'agentPrice', align: 'center', title: '代理价',templet: '#agentPriceEditTpl'
+                // style: 'outline: 1px solid #e6e6e6;outline-offset: -5px;'
+            },
+            {field: 'createTime', align: 'center', width: 160, sort: true, title: '创建时间'},
             {align: 'center', toolbar: '#tableBar', width: 120, fixed: 'right', title: '操作'}
         ]];
     };
-    //监听单元格编辑
-    table.on('edit(card)', function(obj){
-        console.log(obj)
-        var value = obj.value //得到修改后的值
-            ,data = obj.data //得到所在行所有键值
-            ,field = obj.field; //得到字段
-        layer.msg('[ID: '+ data.cardTypeId +'] ' + field + ' 字段更改为：'+ value);
-    });
+
     // 渲染表格
     var tableResult = table.render({
         elem: '#' + AgentApp.card,
@@ -131,15 +126,37 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
         defaultToolbar: [],
         height: "full-126",
         cellMinWidth: 100,
-        where:{
-            'cardType':0,
-            'agentAppId':Feng.getUrlParam("agentAppId")
+        where: {
+            'cardType': 0,
+            'agentAppId': Feng.getUrlParam("agentAppId")
         },
         cols: AgentApp.initColumn(),
     });
     //让当前iframe弹层高度适应
     admin.iframeAuto();
 
+    //监听单元格编辑
+    // table.on('edit(card)', function (obj) {
+    //     console.log(obj)
+    //     var value = obj.value //得到修改后的值
+    //         , data = obj.data //得到所在行所有键值
+    //         , field = obj.field; //得到字段
+    //     if (obj.event =="cellClick"){
+    //         $("table input").attr("type","number");
+    //         $("table input").attr("step","0.1");
+    //     }
+    //     layer.msg('[ID: ' + data.cardTypeId + '] ' + field + ' 字段更改为：' + value);
+    // });
+    //监听行工具条
+    // table.on('tool(card)', function (obj) {
+    //     console.log(obj.event)
+    //     switch (obj.event) {
+    //         case 'cellClick':
+    //             CellClick(this, obj);
+    //             break;
+    //     }
+    //     ;
+    // });
     /**
      * 初始化单码卡类
      */
@@ -148,36 +165,7 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
         var initializeItem = function () {
             var ajax = new $ax(Feng.ctxPath + "/agentCard/initializeItemCodeCard", function (data) {
                 Feng.success("初始化成功!");
-                if (cardType==0){
-                    table.reload(AgentApp.card);
-                }
-                // else if (cardType==1){
-                //     table.reload(AgentApp.currentCard);
-                // }
-                else {
-                    table.reload(AgentApp.account);
-                }
-
-            }, function (data) {
-                Feng.error("初始化失败!" + data.responseJSON.message + "!");
-            });
-            ajax.set("cardType", cardType);
-            ajax.set("agentAppId", Feng.getUrlParam("agentAppId"));
-            ajax.set("appId",Feng.getUrlParam("appId"));
-            ajax.start();
-        };
-        Feng.confirm("是否初始化?", initializeItem);
-    };
-
-    /**
-     * 初始化注册码卡类
-     */
-    AgentApp.initializeItemAccountCard = function (cardType) {
-        console.log(Feng.getUrlParam("appId"));
-        var initializeItem = function () {
-            var ajax = new $ax(Feng.ctxPath + "/agentCard/initializeItemAccountCard", function (data) {
-                Feng.success("初始化成功!");
-                if (cardType==0){
+                if (cardType == 0) {
                     table.reload(AgentApp.card);
                 }
                     // else if (cardType==1){
@@ -192,7 +180,36 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
             });
             ajax.set("cardType", cardType);
             ajax.set("agentAppId", Feng.getUrlParam("agentAppId"));
-            ajax.set("appId",Feng.getUrlParam("appId"));
+            ajax.set("appId", Feng.getUrlParam("appId"));
+            ajax.start();
+        };
+        Feng.confirm("是否初始化?", initializeItem);
+    };
+
+    /**
+     * 初始化注册码卡类
+     */
+    AgentApp.initializeItemAccountCard = function (cardType) {
+        console.log(Feng.getUrlParam("appId"));
+        var initializeItem = function () {
+            var ajax = new $ax(Feng.ctxPath + "/agentCard/initializeItemAccountCard", function (data) {
+                Feng.success("初始化成功!");
+                if (cardType == 0) {
+                    table.reload(AgentApp.card);
+                }
+                    // else if (cardType==1){
+                    //     table.reload(AgentApp.currentCard);
+                // }
+                else {
+                    table.reload(AgentApp.account);
+                }
+
+            }, function (data) {
+                Feng.error("初始化失败!" + data.responseJSON.message + "!");
+            });
+            ajax.set("cardType", cardType);
+            ajax.set("agentAppId", Feng.getUrlParam("agentAppId"));
+            ajax.set("appId", Feng.getUrlParam("appId"));
             ajax.start();
         };
         Feng.confirm("是否初始化?", initializeItem);
@@ -203,7 +220,7 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
      *
      * @param data 点击按钮时候的行数据
      */
-    AgentApp.openEditDlg = function (data,cardType) {
+    AgentApp.openEditDlg = function (data, cardType) {
         admin.putTempData('formOk', false);
         top.layui.admin.open({
             type: 2,
@@ -211,11 +228,11 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
             area: '500px',
             content: Feng.ctxPath + '/agentCard/edit?agentCardId=' + data.agentCardId + '&cardType=' + cardType,
             end: function () {
-                if (cardType==0){
+                if (cardType == 0) {
                     admin.getTempData('formOk') && table.reload(AgentApp.card);
-                }else if (cardType==1){
+                } else if (cardType == 1) {
                     admin.getTempData('formOk') && table.reload(AgentApp.currentCard);
-                }else {
+                } else {
                     admin.getTempData('formOk') && table.reload(AgentApp.account);
                 }
             }
@@ -223,11 +240,11 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
     };
 
     // 单码卡密表头工具条点击事件
-    table.on('toolbar(' + AgentApp.card + ')', function(obj){
+    table.on('toolbar(' + AgentApp.card + ')', function (obj) {
         //添加
-        if(obj.event === 'btnAdd'){
+        if (obj.event === 'btnAdd') {
             AgentApp.openAddDlg();
-        } else if(obj.event === 'initialize'){
+        } else if (obj.event === 'initialize') {
             AgentApp.initializeItemCodeCard(0);
         }
     });
@@ -241,11 +258,11 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
     //     }
     // });
     // 账号卡密表头工具条点击事件
-    table.on('toolbar(' + AgentApp.account + ')', function(obj){
+    table.on('toolbar(' + AgentApp.account + ')', function (obj) {
         //添加
-        if(obj.event === 'btnAdd'){
+        if (obj.event === 'btnAdd') {
             AgentApp.openAddDlg();
-        } else if(obj.event === 'initialize'){
+        } else if (obj.event === 'initialize') {
             AgentApp.initializeItemAccountCard(2);
         }
     });
@@ -254,12 +271,79 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
     table.on('tool(' + AgentApp.card + ')', function (obj) {
         var data = obj.data;
         var layEvent = obj.event;
+        console.log(layEvent);
         if (layEvent === 'edit') {
-            AgentApp.openEditDlg(data,0);
+            AgentApp.openEditDlg(data, 0);
         } else if (layEvent === 'delete') {
-            AgentApp.onDeleteItem(data,0);
+            AgentApp.onDeleteItem(data, 0);
+        }
+        else if (layEvent === 'cellClick1') {
+            $(this).on('input porpertychange',function(e){
+                var val= $(this).val();
+                var name= $(this).attr('name');
+                data[name]=val
+                // obj.update(data);//更新行对象数据
+                console.log(obj);
+            })
+
+            // var flag = false;
+            // $(this).on('input', function () {
+            //     if (!flag)
+            //         console.log($(this).val());
+            // }).on('compositionstart', function () {
+            //     flag = true;
+            //     console.log('输入法，录入开始');
+            // }).on('compositionend', function () {
+            //     flag = false;
+            //     console.log('输入法，输入结束--'+$(this).val());
+            // });
         }
     });
+    function CellClick(that,obj){
+        //当前点击字段
+        var field = $(that).data("field");
+        console.log(field)
+        //判断是否需要添加编辑框
+        if(field=="edit")return true;
+        //当前行数据
+        var data = obj.data;
+        console.log(data)
+        //当前单元格的值
+        var value = data[field];
+        console.log(value)
+        //当前点击td的宽高
+        var height = $(that)[0].offsetHeight,width = $(that)[0].offsetWidth;
+        //当前点击td的坐标
+        var top = $(that).offset().top,left = $(that).offset().left;
+
+        //输入框 这里可以自定义表单内容
+        var input = '<input type="number" class="layui-input" value="'+value+'" id="'+field+'_input" data-field="'+field+'" style="width:'+width+'px;height:'+height+'px">';
+        //弹出层
+        layer.open({
+            type: 1
+            ,title:false
+            ,page:true
+            ,limit:1
+            ,closeBtn:0
+            ,area: [width+"px", height+"px"]
+            ,shade: [0.01, '#fff']
+            ,shadeClose:true
+            ,content: input //这里content是一个普通的String
+            ,offset:[top,left]
+            ,success:function(){
+                //使弹出层相对定位
+                $(".layui-layer-page").css("position","absolute")
+                //设置输入框的值
+                $("#"+field+"_input").focus();
+                $("#"+field+"_input").val(value);
+                $("#"+field+"_input").blur(function(){
+                    //同步更新缓存对应的值
+                    data[field] = $(this).val();
+                    obj.update(data);
+                })
+            }
+        });
+    }
 
     // 行内工具条点击事件
     // table.on('tool(' + AgentApp.currentCard + ')', function (obj) {
@@ -277,9 +361,9 @@ layui.use(['table', 'form', 'formX', 'admin', 'ax', 'element'], function () {
         var data = obj.data;
         var layEvent = obj.event;
         if (layEvent === 'edit') {
-            AgentApp.openEditDlg(data,2);
+            AgentApp.openEditDlg(data, 2);
         } else if (layEvent === 'delete') {
-            AgentApp.onDeleteItem(data,2);
+            AgentApp.onDeleteItem(data, 2);
         }
     });
 
